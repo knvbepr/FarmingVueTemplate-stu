1142e27bd887297c949ed378e78a0de6387cb451
knvbepr <3366039503@qq.com>
2025-05-13 16:01:19 +0800

    费用支出汇总

--- src/components/views/condition/expense/costSummary.vue
+++ src/components/views/condition/expense/costSummary.vue
@@ -11,5 +11,55 @@
 				</el-breadcrumb-item>
 			</el-breadcrumb>
 		</div>
+
+        <div class="container">
+            <el-table :data="tableData" border class="table" header-cell-class-name="table-header">
+                <el-table-column prop="name" label="名称" align="center"></el-table-column>
+                <el-table-column prop="value" label="支出金额" align="center"></el-table-column>
+                <el-table-column prop="proportion" label="占比" align="center"></el-table-column>
+            </el-table>
+            <eCost class="etable"></eCost>
+        </div>
     </div>
 </template>
+
+<script>
+import axios from 'axios';
+import eCost from '@/components/views/eTables/eCostSummary';
+export default {
+    name:'AgriculturalProductManagement',
+    data(){
+        return{
+            tableData:[]
+        }
+    },
+    components:{
+        eCost
+    },
+    created() {
+        this.getData();
+    },
+    methods: {
+        getData() {
+            axios.get("/costSummary.json").then(res => {
+                console.log(res.data);
+                this.tableData = res.data;
+            }).catch((error) => {
+                console.error('数据获取失败：',error);
+                this.$message.error('获取数据失败，请稍后重试')
+            });
+        }
+    }
+}
+</script>
+
+<style scoped>
+.table {
+    font-size: 14px;
+    width: 45%;
+    display: inline-block;
+}
+.etable {
+    display: inline-block;
+}
+</style>
